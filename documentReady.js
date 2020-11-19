7710adbb958954c4fc231266675e2d91fba607c2
JoseSachango <62193290+JoseSachango@users.noreply.github.com>
2020-11-19 15:22:15 -0600

    Adding comments and deleting unnecessary code

--- documentReady.js
+++ documentReady.js
@@ -2,13 +2,15 @@
 $(document).ready(function(){
     let $searchForm = $('#searchForm');
 
+    //event listener that takes the value submitted by the form and passes it as an argument to the handleSearch function
     $searchForm.submit(function(event){
         event.preventDefault();
         let searchBreed = $('.autocomplete').val();
-        //console.log(searchBreed);
+       
         handleSearch(searchBreed);
     });
 
+    //event listener that allows us to select a value to submit 
     $searchForm.on('click', '.autocomplete-content', function () {
         let searchBreed = $('.autocomplete').val();
         handleSearch(searchBreed);
